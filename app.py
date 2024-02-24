2df5051f35eacfd01cb7fa3d425c38e1613142c1
Aldorax <109093997+Aldorax@users.noreply.github.com>
2024-02-24 14:17:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,9 @@ def add_cors_headers(response):
         'http://localhost:3000',
         'https://www.enetworksagencybanking.com.ng',
         'https://enetworks-update.vercel.app',
-        'https://jobs-admin.vercel.app'
+        'https://jobs-admin.vercel.app',
+        'http://enetworksoffice.com.ng/',
+        'https://enetworksoffice.com.ng/'
     ]
 
     origin = request.headers.get('Origin')
